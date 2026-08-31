523b3870d365a78df6d934e6190e136f489f6223
Gabriel Koch da Silva <gabrielkochsilva@gmail.com>
2026-08-31 12:06:56 -0300

    iniciando exercico meses do ano

--- src/main/java/org/gabriel/pessoal/lista01/CalcDataAnoBissexto.java
+++ src/main/java/org/gabriel/pessoal/lista01/CalcDataAnoBissexto.java
@@ -1,6 +1,35 @@
 package org.gabriel.pessoal.lista01;
 
+import java.util.Scanner;
+
 public class CalcDataAnoBissexto {
+
+    private static int dezena (int num){
+        return ((num/10) % 10) * 10;
+    }
+
+    private static int unidade (int num){
+        return num % 10;
+    }
+
+    private static boolean eBissexto(int ano){
+        int u = unidade(ano);
+        int d = dezena(ano);
+        int lastTwoDigits = d + u;
+
+        if (lastTwoDigits == 0) {
+            return ano % 400 == 0;
+            // 2000 % 400 == 0
+            // 1900 % 400 == 300
+        } else {
+            return lastTwoDigits % 4 == 0;
+            // 2024 % 4 == 0
+            // 2026 % 4 == 2
+        }
+    }
+
+
+
     /**
     * Ano Bissexto e Diferença de Dias <br>
     * Sem usar nenhuma classe de data (LocalDate, Calendar, etc. — isso vem bem mais na frente do curso):<br><br>
@@ -10,6 +39,20 @@ public class CalcDataAnoBissexto {
     */
     public static void main(String[] args) {
         //==== Exercício 9 ====
-        System.out.println("Ano bissexto e diferença de dias");
+        int[] meses = {31,28,31,30,31,30,31,31,30,31,30,31};
+        Scanner input = new Scanner(System.in);
+        System.out.println("Informe um ano: ");
+        int ano = input.nextInt();
+        System.out.println("Informe um mês (o número, não o nome)");
+        int mes = input.nextInt();
+        System.out.println("");
+        int dia = input.nextInt();
+        if (eBissexto(ano)){
+            meses[1] += 1;
+            mes--; // Para ir de acordo com os índices do array, caso o user informe Janeiro (01), decrementamos 1 para ir para o índice zero
+        } else {
+
+        }
+
     }
 }
