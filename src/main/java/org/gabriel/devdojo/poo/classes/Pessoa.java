cf6f9529d2f2aac0b34f2c48413230da49b45397
Gabriel Koch da Silva <gabrielkochsilva@gmail.com>
2026-09-05 14:59:08 -0300

    coesão e acoplamento

---
+++ src/main/java/org/gabriel/devdojo/poo/classes/Pessoa.java
@@ -0,0 +1,44 @@
+package org.gabriel.devdojo.poo.classes;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    /**
+     * Acoplamento = define o quanto uma classe tem acesso e permissões sobre outra, alto acoplamento indica
+     * que uma classe modifica com muita facilidade estados e atributos de outra;
+     * <br><br>
+     * Coesão = grau de responsabilidade de uma classe, classes devem ter responsabilidades
+     * condizentes com o domínio da mesma;<br><br>
+     *
+     * Baixo Acoplamento = bom<br><br>
+     * Alto Acoplamento = ruim<br><br>
+     * Baixa Coesão = ruim<br><br>
+     * Alta Coesão = bom
+     * */
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setIdade(int idade) {
+        if (idade < 0) {
+            System.out.println("Idade inválida!");
+            return; // return vazio para funcionar com void e 'escapar' da função
+        }
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+    }
+}
---
+++ src/main/java/org/gabriel/devdojo/poo/testesclasses/testePessoa.java
@@ -0,0 +1,12 @@
+package org.gabriel.devdojo.poo.testesclasses;
+import org.gabriel.devdojo.poo.classes.Pessoa;
+
+public class testePessoa{
+    public static void main(String[] args) {
+
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Gabriel");
+        pessoa.setIdade(-2);
+        pessoa.setIdade(19);
+    }
+}
